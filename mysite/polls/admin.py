d0c79aa65f9103be3a4a6a8e5e8c1b5b2dcd86b9
arav jain <aravjain63@gmail.com>
2019-08-20 02:35:08 +0530

    changed djnango admin//admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Question, Choice
+""""
+to add choices directly to one question,removing,admin.site.register(choice)
+"""
+class ChoiceInline(admin.TabularInline):            #rep;ace tabular by stack
+    model = Choice
+    extra = 1
 
-admin.site.register(Question)
-#admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("LOL",               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+                ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+admin.site.register(Question, QuestionAdmin)
+
 
 
 # Register your models here.
